d4cc4b2c8b29783484728e3b60467c16d75377ea
Antonio-CMota <97623339+Antonio-CMota@users.noreply.github.com>
2024-10-28 04:23:48 +0000

    2312

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="bg-charcoal-gray p-4 fixed top-0 left-0 w-full z-50"> {/* Fixed at the top with full width */}
+        <nav className="bg-charcoal-gray p-4  top-0 left-0 w-full z-50"> {/* Fixed at the top with full width */}
             <ul className="flex justify-end space-x-8">
                 <li>
                     <Link className="text-light-gray hover:text-slate-gray" to="/">Home</Link>
